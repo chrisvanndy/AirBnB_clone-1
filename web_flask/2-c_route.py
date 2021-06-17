0aaaeb1b8bdbb464b45e9428395da33f0cbb477f
chrisvanndy <chrisvannyc@gmail.com>
2021-06-17 21:46:27 +0000

    use variable is route def

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This module builds a script that starts a Flask app"""
+# web application must be listening on 0.0.0.0
+# port must be set to 5000
+# Routes: / : display "Hello HBNB!"
+# must use the option strict_slashes=False in route definition
+
+from flask import Flask
+HBNB = Flask(__name__)
+HBNB.url_map.strict_slashes = False
+
+
+@HBNB.route('/')
+def hello():
+    """hello is route definition for '/'"""
+    return 'Hello HBNB!'
+
+
+@HBNB.route('/hbnb')
+def hello2():
+    """hello2 is route definition for '/hbnb'"""
+    return 'HBNB'
+
+
+@HBNB.route('/c/<text>')
+def text_variable(text):
+    return 'C %s' % text
+
+if __name__ == '__main__':
+    HBNB.run(debug=True, host='0.0.0.0', port=5000)
