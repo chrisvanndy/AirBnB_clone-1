d5a4caf8a55e7cbc8d558776553fc3bced4ad950
chrisvanndy <chrisvannyc@gmail.com>
2021-06-17 21:20:36 +0000

    additional route def: hello2

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module builds a script that starts a Flask app"""
+# web application must be listening on 0.0.0.0
+# port must be set to 5000
+# Routes: / : display "Hello HBNB!"
+# must use the option strict_slashes=False in route definition
+
+from flask import Flask
+HBNB = Flask(__name__)
+HBNB.url_map.strict_slashes = False
+
+
+@HBNB.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+
+@HBNB.route('/hbnb'):
+def hello2():
+    return 'HBNB'
+
+if __name__ == '__main__':
+    HBNB.run(host='0.0.0.0')
