284f28ebf31ee494159e8d5f07ae7c674eb307d7
chrisvanndy <chrisvannyc@gmail.com>
2021-06-18 20:19:21 +0000

    adding state Flask

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Create Flask for State objects"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+STATES = Flask(__name__)
+STATES.url_map.strict_slashes = False
+
+ST = '7-states_list.html'
+
+@STATES.route('/states_list')
+def dispalySates()
+    return render_template(ST, State=storage.all(State)[name].sort())
+
+if __name__ == '__main__':
+    STATES.run(debug=True, host='0.0.0.0', port=5000)
---
+++ web_flask/templates/7-states_list.py
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<HTML lang="en">
+    <HEAD>
+        <TITLE>HBNB</TITLE>
+    </HEAD>
+    <BODY>
+        <H1>States</H1>
+        <UL id="State">
+        {% for item in State %}
+            <LI>"{{ item.id  }}: "><B>"{{ item.name }}"</B></LI>
+        {% endfor %}
+        </UL>
+    </BODY>
+</HTML>
+
